558dd70fe22d0c026c72ce580cd010c798f6f7d7
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-03-14 22:42:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (drivers, target) {
+    return drivers.filter(possibleMatch => possibleMatch.toLowerCase() === target.toLowerCase());
+}
+
+function fuzzyMatch (drivers, target) {
+    return drivers.filter(possibleMatch => possibleMatch.toLowerCase().indexOf(target.toLowerCase()) === 0 )
+}
+
+function matchName (driverArray, string) {
+  return driverArray.filter(possibleMatch => possibleMatch.name === string)
+}
